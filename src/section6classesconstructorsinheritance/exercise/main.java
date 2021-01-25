70fc80610d2a5711cd32887d5e6c2f95497d6e2a
SofHldn <sofie.holden@iths.se>
2021-01-25 08:43:10 +0100

    section6 inheritance

---
+++ src/section6classesconstructorsinheritance/exercise/main.java
@@ -0,0 +1,12 @@
+package section6classesconstructorsinheritance.exercise;
+
+public class main {
+
+    public static void main(String[] args) {
+        Cylinder cylinder = new Cylinder(3.0, 2);
+
+        System.out.println(cylinder.getVolume());
+
+    }
+
+}
